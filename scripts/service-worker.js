b4f39ec8af4ed780aa46c6ed1e57667c8d67b334
moonsfrost <mydsqwe@foxmail.com>
2024-12-03 19:27:36 +0800

    增加定时回到学术模式的功能

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -9,7 +9,9 @@ async function GetCurrentTab() {
 }
 
 function modeShift(flag){
-    if(flag===0){
+    if(flag===0){ //Academic mode
+        console.log("switch to aca mode");
+        // change injected scripts
         chrome.scripting.registerContentScripts([{
             id: "mainPageForAca",
             js: ["scripts/listMaker.js","scripts/mainPageProcesser.js"],
@@ -24,10 +26,23 @@ function modeShift(flag){
             matches: [videoPageUrl+"*"],
             persistAcrossSessions: false,
             runAt: "document_end"
-        }])
+        }]);
+        // remove alarms
+        chrome.alarms.clear("limitRecreation");
+        chrome.storage.local.set({["alarmBeginTime"]: -1});
     }
-    else if(flag===1){
+    else if(flag===1){ // Limited Recreation mode
+        console.log("switch to rec mode");
+        // change injected scripts
         chrome.scripting.unregisterContentScripts({ids: ["mainPageForAca","videoPageForAca"]});
+        // set alarms
+        let d=new Date;
+        chrome.storage.local.set({alarmBeginTime: d.getTime()});
+        chrome.storage.local.get("limitTime").then((item)=>{
+            chrome.alarms.create("limitRecreation",{
+                delayInMinutes: item.limitTime
+            })
+        })
     }
 }
 
@@ -41,7 +56,7 @@ async function searchPageAcademic(tab){
 
 chrome.storage.onChanged.addListener((chg,area)=>{
     for(let [key,{oldValue, newValue}] of Object.entries(chg)){
-        if(key==="mode") modeShift(newValue);
+        if(key==="mode"&&oldValue!==newValue) modeShift(newValue);
     }
 })
 
@@ -49,5 +64,43 @@ chrome.runtime.onInstalled.addListener(()=>{
     chrome.storage.local.clear().then(()=>{
         chrome.storage.local.set({["lists"]:[]});
         chrome.storage.local.set({["mode"]: 0});
-    })
+        chrome.storage.local.set({["alarmBeginTime"]: -1});
+        chrome.storage.local.set({["limitTime"]: 1});
+    });
+    chrome.alarms.clearAll();
 })
+
+chrome.alarms.onAlarm.addListener((alm)=>{
+    chrome.storage.local.set({["mode"]: 0});
+    console.log(alm.name);
+})
+
+setTimeout(()=>{},500);
+// make sure addListener is the priority
+
+chrome.storage.local.get("mode").then((item)=>{
+    console.log(item.mode===0?"aca":"rec");
+});
+async function recoverAlarm(){
+    let item= await chrome.storage.local.get("alarmBeginTime");
+    let lim= await chrome.storage.local.get("limitTime");
+    lim=lim.limitTime;
+    console.log("beginProcess");
+    if(item === undefined || item.alarmBeginTime === -1) return;
+    chrome.alarms.get("limitRecreation", (alm) =>{
+        if(!alm){
+            console.log("recovering");
+            let d=new Date;
+            let now=d.getTime();
+            let beg=item.alarmBeginTime;
+            let left=Math.floor(beg+lim*60*1000-now);
+            if(left<=0) chrome.storage.local.get("mode").then((item)=>{
+                if(item.mode===1) chrome.storage.local.set({mode: 0});
+            });
+            else chrome.alarms.create("limitRecreation",{
+                delayInMinutes: left
+            });
+        }
+    })
+}
+recoverAlarm();
